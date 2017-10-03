1f05063e4255988bcb79650a4b1793e79dd1a89a
olga-nest <olga.w.nesterova@gmail.com>
2017-10-03 11:31:07 -0700

    Calculate volume of a box

--- main.m
+++ main.m
@@ -1,7 +1,12 @@
 #import <Foundation/Foundation.h>
+#import "Box.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+
+        Box *boxObj = [[Box alloc]initWithHeight:100 andWidth:77 andLength:200];
+        float volume = [boxObj calculateVolume];
+        NSLog(@"The volume on the box is: %f", volume);
         
     }
     return 0;
